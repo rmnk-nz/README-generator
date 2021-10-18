15206b2eab90464bf3f87d0b61a42298f32cec94
rmnk-nz <rua.manukau20@gmail.com>
2021-10-18 12:46:19 +1100

    installed inquirer and questions to be asked

--- index.js
+++ index.js
@@ -1,7 +1,64 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs'); 
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'projectName',
+        message: 'What is the project title?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Write a description of your project',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What is required to install your project?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        messgae: 'Provide usage information for project'
+
+    },
+    {
+        type: 'list',
+        name: 'license',
+        choices: [
+            'MIT',
+            'GNU',
+            'Mozilla',
+            'Apache',
+            'ISC',
+        ]
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Include contributing guidelines for project',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Include test Instructions for the project',
+    },
+    {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'Enter your Github Username'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter email address'
+    }
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
